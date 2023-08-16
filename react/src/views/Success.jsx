c23c99711e75aa84eeb0452a68bf35981d03e740
Njeri <85888329+Njeri-Ngugi@users.noreply.github.com>
2023-08-16 13:44:40 +0300

    Multiple minor changes made

--- react/src/views/Success.jsx
+++ react/src/views/Success.jsx
@@ -5,7 +5,7 @@ const Success = () => {
     return (
         <>
             <div className="SuccessDiv" style={{paddingTop: "200px"}}>
-                <h1 style={{color: "#00bd05", fontWeight: "700", textAlign: "center"}} className="SuccessMessage">
+                <h1 style={{color: "#00bd05", fontWeight: "700", textAlign: "center", fontSize: "28px"}} className="SuccessMessage">
                     Your data has been submitted successfully!
                 </h1>
             </div>
